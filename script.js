a0d8fefe083c2d15ec0bb3cf66dec1e671f466d4
Bri <bhilliker@gmail.com>
2021-07-02 09:34:21 -0500

    added time-block color

--- script.js
+++ script.js
@@ -1,89 +1,65 @@
 document.getElementById("currentDay").innerHTML =
   moment().format("dddd, MMMM do YYYY");
 
-var tasks = JSON.parse(localStorage.getItem("tasks"));
+const textArea = document.querySelector("textarea").value;
+console.log("line 5", textArea);
 
-if (!tasks) {
-  tasks = [
-    {
-      time: "8a",
-      task: "",
-    },
-    {
-      time: "9a",
-      task: "",
-    },
-    {
-      time: "10a",
-      task: "",
-    },
-    {
-      time: "11a",
-      task: "",
-    },
-    {
-      time: "12p",
-      task: "",
-    },
-    {
-      time: "1p",
-      task: "",
-    },
-    {
-      time: "2p",
-      task: "",
-    },
-    {
-      time: "3p",
-      task: "",
-    },
-    {
-      time: "4p",
-      task: "",
-    },
-    {
-      time: "5p",
-      task: "",
-    },
-  ];
-}
+// pass the textArea into the tasks array
 
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
-// ----------------TODO------------------
-
-// time block allows click and event -------------------- addEventListener
-var $now = document.getElementsByClassName(".row");
-var $hour = document.getElementsByClassName("div.hour");
-console.log(moment().isBefore("hour"));
 
 var timeBlock = document.getElementsByClassName("time-block");
-console.log(timeBlock);
+console.log(timeBlock)
 
-for (let i = 0; i < timeBlock.length; i++) {
-  // var now = moment()
-  var now = moment("12", "h");
-  console.log(now);
-  const element = timeBlock[i];
-  console.log(element.id);
-  var parse = moment(element.id, "hA");
-  console.log(parse.isBefore(now));
-  if (parse.isBefore(moment("12", "h"))) {
-    console.log("cats hate dogs");
-    element.classList.add("past");
-  } else if (parse.isSame(now, "hour")) {
-    element.classList.add("present");
-  } else if (parse.isAfter(now)) {
-    element.classList.add("future");
-  } else {
-    console.log("none of the above");
+
+var tasks = JSON.parse(localStorage.getItem("tasks"));
+console.log("does it pass through tasks?");
+
+function loadTasks() {
+  tasks = localStorage.getItem("tasks");
+  console.log(savedTasks);
+  if (!tasks) {
+    tasks = [
+      {
+        time: "8a",
+        task: "",
+      },
+      {
+        time: "9a",
+        task: "",
+      },
+      {
+        time: "10a",
+        task: "",
+      },
+      {
+        time: "11a",
+        task: "",
+      },
+      {
+        time: "12p",
+        task: "",
+      },
+      {
+        time: "1p",
+        task: "",
+      },
+      {
+        time: "2p",
+        task: "",
+      },
+      {
+        time: "3p",
+        task: "",
+      },
+      {
+        time: "4p",
+        task: "",
+      },
+      {
+        time: "5p",
+        task: "",
+      },
+    ];
   }
 }
 
@@ -91,23 +67,34 @@ $(".saveBtn").on("click", saveTask);
 
 function saveTask() {
   console.log($(this).siblings(".description").val());
-
-  localStorage.setItem("tasks", JSON.stringify($(this) , tasks));
+  console.log ("_____________________________________________________________")
+  localStorage.setItem("tasks", JSON.stringify(tasks));
   console.log(tasks);
-  console.log("ice cream");
 }
 
-// $(".description").on("blur", "textarea", function(){
-//   // recreate p element
-//   var taskP = $("<p>")
-// },
-// add save buttons to grid ----------------- localStorage
-//     JSON.stringify()  (puts object into an array)
-//     {
-//         time:
-//         event:
-//     }
 
-// Persistance
-//     JSON.parse(localStorage.getItem("tasks"))
-//     if nothing in localStorage, create a new object to track all "tasks" arrays
+// ----------------TODO------------------
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
+for (let i = 0; i < timeBlock.length; i++) {
+  // var now = moment()
+  var now = moment("12", "h");
+  // console.log(now);
+  const element = timeBlock[i];
+  // console.log(element.id);
+  var parse = moment(element.id, "hA");
+  console.log(parse.isBefore(now));
+  if (parse.isBefore(moment(now, "h"))) {
+    // console.log("cats hate dogs");
+    element.classList.add("past");
+  } else if (parse.isSame(now, "h")) {
+    element.classList.add("present");
+  } else if (parse.isAfter(now, "h")) {
+    element.classList.add("future");
+  } else {
+    // console.log("none of the above");
+  }
+}
