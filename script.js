93ca6262d681751044bd4ac26d811b0210b481bd
Bri <bhilliker@gmail.com>
2021-07-03 22:07:38 -0500

    add button stylings

--- script.js
+++ script.js
@@ -1,95 +1,53 @@
 document.getElementById("currentDay").innerHTML =
-  moment().format("dddd, MMMM do YYYY");
-
-const tasks = [];
-const textArea = document.querySelector("textarea").value;
-// console.log("line 5", textArea);
-
-// pass the textArea into the tasks array
+moment().format("dddd, MMMM do YYYY");
 
 var timeBlock = document.getElementsByClassName("time-block");
-// console.log(timeBlock)
-$(".saveBtn").on("click", saveTask);
-
-function saveTask() {
-  tasks.push($(this).siblings(".description").val());
-  // console.log ("_____________________________________________________________")
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  // console.log(tasks);
-}
-
-
-function loadTasks() {
-  var tasks = JSON.parse(localStorage.getItem("tasks"));
-  // console.log("does it pass through tasks?");
-  console.log(tasks);
-  if (!tasks) {
-    savedTasks = [
-      {
-        time: "8a",
-        task: "",
-      },
-      {
-        time: "9a",
-        task: "",
-      },
-      {
-        time: "10a",
-        task: "",
-      },
-      {
-        time: "11a",
-        task: "",
-      },
-      {
-        time: "12p",
-        task: "",
-      },
-      {
-        time: "1p",
-        task: "",
-      },
-      {
-        time: "2p",
-        task: "",
-      },
-      {
-        time: "3p",
-        task: "",
-      },
-      {
-        time: "4p",
-        task: "",
-      },
-      {
-        time: "5p",
-        task: "",
-      },
-    ];
-    return false;
-  }
-  // tasks = JSON.parse(tasks);
-  console.log(tasks)
-}
-
-// ----------------TODO------------------
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+let tasks = JSON.parse(localStorage.getItem("tasks"));
+const textArea = document.querySelector("textarea").value;
 
 for (let i = 0; i < timeBlock.length; i++) {
-  var now = moment().hour();
-  const element = timeBlock[i];
-  var parse = moment(element.id, "hA");
-  if (parse.isBefore(moment(now, "h"))) {
-    element.classList.add("past");
-  } else if (parse.isSame(moment(), "hour")) {
-    element.classList.add("present");
-  } else if (parse.isAfter(now, "h")) {
-    element.classList.add("future");
-  } else {
-    console.log("none of the above");
-  }
+    var now = moment().hour();
+    const element = timeBlock[i];
+    var parse = moment(element.id, "hA");
+    if (parse.isBefore(moment(now, "h"))) {
+      element.classList.add("past");
+    } else if (parse.isSame(moment(), "hour")) {
+      element.classList.add("present");
+    } else if (parse.isAfter(now, "h")) {
+      element.classList.add("future");
+    } else {
+      console.log("none of the above");
+    }
 }
+  
+$(".saveBtn").on("click", saveTask);
+function saveTask(){
+        const key = $(this).parent().attr('id')
+        const value = $(this).siblings(".description").val();    
+        const updatedTasks = {...tasks, [key]: value}
+        tasks = updatedTasks
+        localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+        console.log(updatedTasks)
+}
+
+function loadTasks(){
+    const updatedTasks = JSON.parse(localStorage.getItem("tasks"));
+    $("#8a-text").text(updatedTasks["8a"])
+    $("#9a-text").text(updatedTasks["9a"])
+    $("#10a-text").text(updatedTasks["10a"])
+    $("#11a-text").text(updatedTasks["11a"])
+    $("#12p-text").text(updatedTasks["12p"])
+    $("#1p-text").text(updatedTasks["1p"])
+    $("#2p-text").text(updatedTasks["2p"])
+    $("#3p-text").text(updatedTasks["3p"])
+    $("#4p-text").text(updatedTasks["4p"])
+    $("#5p-text").text(updatedTasks["5p"])
+
+    if (!tasks){
+        const key = $(this).parent().attr('id')
+        const value = $(this).siblings(".description").val();    
+        tasks = {...tasks, [key]: value}
+    } 
+}
+
 loadTasks();
